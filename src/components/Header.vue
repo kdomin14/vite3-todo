679c02ffc4a1247f8d57771befc81ec9c5981a9b
Kayla <avila.kayla4@gmail.com>
2021-02-11 02:03:36 -0600

    scaffold all

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class = "flex flex-col w-full container">
+      <Title msg = "🚀 Kaylas App 🚀" />
+      <NewTodo />
+    </header>
+</template>
+
+<script setup>
+import Title from './Titles.vue'
+import NewTodo from './NewTodo.vue'
+</script>
